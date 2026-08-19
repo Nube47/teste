87d5a0e45aa369655c0cf04c833e5d89682078d5
Nube47 <carlos.8439532@edu.univali.br>
2026-08-19 19:01:00 -0300

    inserção de aluno na tabela

--- script.js
+++ script.js
@@ -13,22 +13,27 @@ function adicionarAluno(iptNome, iptNota1, iptNota2) {
     }
 }
 
-function insercaoTabela(iptNome, iptNota1, iptNota2, iptMedia, iptSituacao){
-    const containerTabela = document.getElementById(tblAlunos);
-    containerTabela.innerHTML(`
+function insercaoTabela(iptNome, iptNota1, iptNota2){
+    const containerTabela = document.getElementById("tblAlunos");
+    const iptMedia = (Number(iptNota1) + Number(iptNota2)) / 2;
+    let iptSituacao = "Não Verificado";
+
+    if (iptMedia  > 6) iptSituacao = "Aprovado";
+        else iptSituacao = "Reprovado";
+
+    const novaTabela = document.createElement ('tr');
+    novaTabela.innerHTML = `
         <tr>
         <td>${iptNome}</td>
         <td>${iptNota1}</td>
         <td>${iptNota2}</td>
-        <td>${(iptNota1, iptNota2) => {
-            const media = (iptNota1 + iptNota2) / 2;
-        if(media > 6){iptSituacao = "Aprovado"}
-    else {iptSituacao = "Reprovado"}}}</td>
-        <td></td>
+        <td>${iptMedia}</td>
+        <td>${iptSituacao}</td>
         </tr>
-        `)
-}
+        `
 
+    containerTabela.appendChild(novaTabela)
+}
 //botão de cadastrar
 btnCadastrar.addEventListener("click", function () {
     const iptNome = document.querySelector("#iptNome").value;
@@ -36,6 +41,6 @@ btnCadastrar.addEventListener("click", function () {
     const iptNota2 = document.querySelector("#iptNota2").value;
 
     Alunos.push(adicionarAluno(iptNome, iptNota1, iptNota2, ));
-
+    insercaoTabela(iptNome, iptNota1, iptNota2)
 
 })
